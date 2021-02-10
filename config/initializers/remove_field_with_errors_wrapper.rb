2db572204636b114df44eaff99a104ead3643557
Joao Roberto <joaorobertojr88@gmail.com>
2021-02-10 17:29:02 -0300

    Remove field_with_errors wrapper

---
+++ config/initializers/remove_field_with_errors_wrapper.rb
@@ -0,0 +1,5 @@
+# By default, Rails adds div.field_with_errors wrapper to the form field.
+# This initializer removes this wrapper to avoid causing UI issues.
+ActionView::Base.field_error_proc = Proc.new do |html_tag|
+  html_tag
+end
