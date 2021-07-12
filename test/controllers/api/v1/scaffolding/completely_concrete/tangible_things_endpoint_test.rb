89e9823bb3f075bff3739d5413dcb400595e2751
Tim Haines <tmhaines@gmail.com>
2021-07-12 17:58:44 +1200

    Adjust tests to resolve warnings about using assert_nil for Minitest 6 (#204)

--- test/controllers/api/v1/scaffolding/completely_concrete/tangible_things_endpoint_test.rb
+++ test/controllers/api/v1/scaffolding/completely_concrete/tangible_things_endpoint_test.rb
@@ -31,7 +31,13 @@ class Api::V1::Scaffolding::CompletelyConcrete::TangibleThingsEndpointTest < Api
       assert_equal tangible_thing_data["email_field_value"], tangible_thing.email_field_value
       assert_equal tangible_thing_data["password_field_value"], tangible_thing.password_field_value
       assert_equal tangible_thing_data["phone_field_value"], tangible_thing.phone_field_value
-      assert_equal tangible_thing_data["option_value"], tangible_thing.option_value
+
+      if tangible_thing.option_value.nil?
+        assert_nil tangible_thing_data["option_value"]
+      else
+        assert_equal tangible_thing_data["option_value"], tangible_thing.option_value
+      end
+
       assert_equal tangible_thing_data["super_select_value"], tangible_thing.super_select_value
       # assert_equal tangible_thing_data["text_area_value"], tangible_thing.text_area_value
       # assert_equal tangible_thing_data["ckeditor_value"], tangible_thing.ckeditor_value
