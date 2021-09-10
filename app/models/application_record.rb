a82d24d8feaf1671480f0a6c10389b966b28e4eb
Gabriel Zayas <g-zayas@hotmail.com>
2021-09-11 00:23:50 +0900

    Find parent model in #parent_collection (#296)
    * Find parent model in AppliationRecord#parent_collection
    
    * Localize ApplicationRecord#parent_collection error message
    
    * Refactor ApplicationRecord#parent_collection
    
    * Revert to string literal for message in ApplicationRecord#parent_collection

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -47,8 +47,9 @@ class ApplicationRecord < ActiveRecord::Base
   end
 
   def parent_collection
-    # TODO Make this error message use reflection to try to guess which model is the parent (it's usually the first
-    # `belongs_to`) andalso try to suggest what the entire method definition should actually be.
-    raise "You're trying to use a feature that requires #{self.class.name} to have a `collection` method defined that returns the Active Record association that this model belongs to within its parent object."
+    # TODO Try to suggest what the entire method definition should actually be.
+    model_name = self.class
+    parent_key = model_name.reflect_on_all_associations(:belongs_to).first.name
+    raise "You're trying to use a feature that requires #{model_name} to have a `collection` method defined that returns the Active Record association that this model belongs to within its parent object."
   end
 end
