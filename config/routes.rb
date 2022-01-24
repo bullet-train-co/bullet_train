aea5f02c0d5b7a4cbcfc6f39582af732146172d1
Andrew Culver <andrew.culver@gmail.com>
2022-01-24 09:11:13 -0800

    Extracting routes.

--- config/routes.rb
+++ config/routes.rb
@@ -82,26 +82,6 @@ Rails.application.routes.draw do
         # routes for many teams actions and resources are configured in the `bullet_train` gem, but you can add more here.
 
         # add your resources here.
-        unless scaffolding_things_disabled?
-          namespace :scaffolding do
-            namespace :absolutely_abstract do
-              resources :creative_concepts do
-                scope module: "creative_concepts" do
-                  resources :collaborators, only: collection_actions
-                end
-
-                namespace :creative_concepts do
-                  resources :collaborators, except: collection_actions
-                end
-              end
-            end
-            resources :absolutely_abstract_creative_concepts, path: "absolutely_abstract/creative_concepts" do
-              namespace :completely_concrete do
-                resources :tangible_things
-              end
-            end
-          end
-        end
 
         resources :invitations do
           # routes for standard invitation actions and resources are configured in the `bullet_train` gem, but you can add more here.
