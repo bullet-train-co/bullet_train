297e4410dfb8a4fd2de4847c65dea15c5b4426e0
Andrew Culver <andrew.culver@gmail.com>
2022-02-14 20:10:32 -0800

    Allow scaffolding of new attributes onto User, Team, Membership.

---
+++ app/controllers/account/teams_controller.rb
@@ -0,0 +1,23 @@
+class Account::TeamsController < Account::ApplicationController
+  include Account::Teams::ControllerBase
+
+  private
+
+  def permitted_fields
+    [
+      :description,
+      # 🚅 super scaffolding will insert new fields above this line.
+    ]
+  end
+
+  def permitted_arrays
+    {
+      # 🚅 super scaffolding will insert new arrays above this line.
+    }
+  end
+
+  def process_params(strong_params)
+    # 🚅 super scaffolding will insert processing for new fields above this line.
+    strong_params
+  end
+end
---
+++ app/controllers/account/users_controller.rb
@@ -0,0 +1,22 @@
+class Account::UsersController < Account::ApplicationController
+  include Account::Users::ControllerBase
+
+  private
+
+  def permitted_fields
+    [
+      # 🚅 super scaffolding will insert new fields above this line.
+    ]
+  end
+
+  def permitted_arrays
+    {
+      # 🚅 super scaffolding will insert new arrays above this line.
+    }
+  end
+
+  def process_params(strong_params)
+    # 🚅 super scaffolding will insert processing for new fields above this line.
+    strong_params
+  end
+end
