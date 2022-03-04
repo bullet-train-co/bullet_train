d4d88fe577799a1b0241f4a5fa704342b737c185
Andrew Culver <andrew.culver@gmail.com>
2022-03-04 09:35:41 -0800

    Extract base current attributes logic into `bullet_train`.

--- app/models/current.rb
+++ app/models/current.rb
@@ -1,33 +1,4 @@
 # See https://api.rubyonrails.org/classes/ActiveSupport/CurrentAttributes.html for details.
 class Current < ActiveSupport::CurrentAttributes
-  attribute :user, :team, :membership, :ability
-
-  resets do
-    Time.zone = nil
-  end
-
-  def user=(user)
-    super
-
-    if user
-      Time.zone = user.time_zone
-      self.ability = Ability.new(user)
-    else
-      Time.zone = nil
-      self.ability = nil
-    end
-
-    update_membership
-  end
-
-  def team=(team)
-    super
-    update_membership
-  end
-
-  def update_membership
-    self.membership = if user && team
-      user.memberships.where(team: team)
-    end
-  end
+  include CurrentAttributes::Base
 end
