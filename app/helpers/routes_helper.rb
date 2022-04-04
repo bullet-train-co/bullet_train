4acd14841e8043e6b25ba7b0f470adf07afc2a0e
Gabriel Zayas <g-zayas@hotmail.com>
2022-04-04 16:47:35 +0900

    Add routes helper

---
+++ app/helpers/routes_helper.rb
@@ -0,0 +1,7 @@
+module RoutesHelper
+  # This is helpful to have around when working with shallow routes and complicated model namespacing. We don't use this
+  # by default, but sometimes Super Scaffolding will generate routes that use this for `only` and `except` options.
+  def self.collection_actions
+    [:index, :new, :create]
+  end
+end
