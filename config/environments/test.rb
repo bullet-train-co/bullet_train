46a509d9722840d2cb46fd8faebc36af18d7be89
Andrew Culver <andrew.culver@gmail.com>
2022-04-07 20:12:05 -0700

    Enable annotation in test.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
-  # config.action_view.annotate_rendered_view_with_filenames = true
+  config.action_view.annotate_rendered_view_with_filenames = true
 
   # 🚫 DEFAULT BULLET TRAIN CONFIGURATION
   # This section represents the default settings for a Bullet Train application. Your own configuration should be
