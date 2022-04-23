b845ca755de628b34af32bfe3a359bba61d123bc
Gabriel Zayas <g-zayas@hotmail.com>
2022-04-23 15:39:49 +0900

    Remove children pagination logic on team show page

--- app/controllers/account/teams_controller.rb
+++ app/controllers/account/teams_controller.rb
@@ -1,6 +1,5 @@
 class Account::TeamsController < Account::ApplicationController
   include Account::Teams::ControllerBase
-  before_action -> { paginate_children(children_to_paginate) }, only: [:show]
 
   private
 
@@ -15,19 +14,6 @@ class Account::TeamsController < Account::ApplicationController
     {
       # 🚅 super scaffolding will insert new arrays above this line.
     }
-  end
-
-  # We use this method to paginate children in the show view.
-  def paginate_children(models)
-    models.each do |model|
-      set_pagy(model, self)
-    end
-  end
-
-  def children_to_paginate
-    [
-      # 🚅 super scaffolding will insert new fields for pagination above this line.
-    ]
   end
 
   def process_params(strong_params)
