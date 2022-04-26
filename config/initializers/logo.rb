b5f615d5a156d7327db49f1c200b162e69205902
Andrew Culver <andrew.culver@gmail.com>
2022-04-25 21:20:20 -0400

    Create logo.rb

---
+++ config/initializers/logo.rb
@@ -0,0 +1,4 @@
+# This setting determines the target height (in pixels) that `app/asset/logo/logo.png` will be resized to (while
+# maintaining aspect ratio) when being presented in the `box` component header or the header of outgoing emails.
+# You may want to play with this setting after replacing the default Bullet Train starter kit logo with something else.
+BulletTrain::Themes.logo_height = 54
