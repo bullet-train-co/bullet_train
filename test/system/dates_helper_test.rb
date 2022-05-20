ddb02f0b3b7c96be7b49ad111f9fd84a44a571fc
Gabriel Zayas <g-zayas@hotmail.com>
2022-05-20 18:51:19 +0900

    Skip certain tests if HIDE_THINGS is set to true

--- test/system/dates_helper_test.rb
+++ test/system/dates_helper_test.rb
@@ -1,51 +1,53 @@
 require "application_system_test_case"
 
 class DatesHelperTest < ApplicationSystemTestCase
-  @@test_devices.each do |device_name, display_details|
-    test "Time is displayed correctly" do
-      resize_for(display_details)
-      be_invited_to_sign_up
-      visit root_path
+  unless scaffolding_things_disabled?
+    @@test_devices.each do |device_name, display_details|
+      test "Time is displayed correctly" do
+        resize_for(display_details)
+        be_invited_to_sign_up
+        visit root_path
 
-      # Sign up and log in
-      sign_up_from_homepage_for(display_details)
-      fill_in "Your Email Address", with: "bullettrain@gmail.com"
-      fill_in "Set Password", with: example_password
-      fill_in "Confirm Password", with: example_password
-      click_on "Sign Up"
-      fill_in "First Name", with: "Testy"
-      fill_in "Last Name", with: "McTesterson"
-      fill_in "Your Team Name", with: "The Testing Team"
-      select "(GMT+00:00) UTC", from: "Your Time Zone"
-      click_on "Next"
+        # Sign up and log in
+        sign_up_from_homepage_for(display_details)
+        fill_in "Your Email Address", with: "bullettrain@gmail.com"
+        fill_in "Set Password", with: example_password
+        fill_in "Confirm Password", with: example_password
+        click_on "Sign Up"
+        fill_in "First Name", with: "Testy"
+        fill_in "Last Name", with: "McTesterson"
+        fill_in "Your Team Name", with: "The Testing Team"
+        select "(GMT+00:00) UTC", from: "Your Time Zone"
+        click_on "Next"
 
-      # Create a Tangible Thing
-      click_on "Creative Concepts"
-      click_on "Add New Creative Concept"
-      fill_in "Name", with: "Test Concept"
-      fill_in "Description", with: "Test Description"
-      click_on "Create Creative Concept"
-      click_on "Add New Tangible Thing"
-      fill_in "Text Field Value", with: "Test Tangible Thing"
-      click_on "Create Tangible Thing"
-      assert page.has_text? "Tangible Thing was successfully created."
+        # Create a Tangible Thing
+        click_on "Creative Concepts"
+        click_on "Add New Creative Concept"
+        fill_in "Name", with: "Test Concept"
+        fill_in "Description", with: "Test Description"
+        click_on "Create Creative Concept"
+        click_on "Add New Tangible Thing"
+        fill_in "Text Field Value", with: "Test Tangible Thing"
+        click_on "Create Tangible Thing"
+        assert page.has_text? "Tangible Thing was successfully created."
 
-      time = Scaffolding::CompletelyConcrete::TangibleThing.first.created_at
+        time = Scaffolding::CompletelyConcrete::TangibleThing.first.created_at
 
-      # Assert today's date is displayed correctly.
-      assert page.has_text? "Today at #{time.strftime("%l:%M %p").strip}"
+        # Assert today's date is displayed correctly.
+        assert page.has_text? "Today at #{time.strftime("%l:%M %p").strip}"
 
-      # Assert yesterday's date is displayed correctly.
-      travel_to time + 1.day
-      visit current_url # Refresh the page
-      assert page.has_text? "Yesterday at #{(time).strftime("%l:%M %p").strip}"
+        # Assert yesterday's date is displayed correctly.
+        travel_to time + 1.day
+        visit current_url # Refresh the page
+        assert page.has_text? "Yesterday at #{(time).strftime("%l:%M %p").strip}"
 
-      # Assert the month and day is shown for anything before then.
-      travel_to time + 2.days
-      visit current_url
-      assert page.has_text? "#{time.strftime("%B %-d").strip} at #{time.strftime("%l:%M %p").strip}"
+        # Assert the month and day is shown for anything before then.
+        travel_to time + 2.days
+        visit current_url
+        assert page.has_text? "#{time.strftime("%B %-d").strip} at #{time.strftime("%l:%M %p").strip}"
 
-      # TODO: Write test for a user with a different time zone
+        # TODO: Write test for a user with a different time zone
+      end
     end
   end
 end
--- test/system/pagination_test.rb
+++ test/system/pagination_test.rb
@@ -7,28 +7,30 @@ class PaginationTest < ApplicationSystemTestCase
     @team = @jane.current_team
   end
 
-  test "pagination works properly" do
-    display_details = @@test_devices[:macbook_pro_15_inch]
-    resize_for(display_details)
+  unless scaffolding_things_disabled?
+    test "pagination works properly" do
+      display_details = @@test_devices[:macbook_pro_15_inch]
+      resize_for(display_details)
 
-    login_as(@jane, scope: :user)
-    visit account_team_path(@jane.current_team)
+      login_as(@jane, scope: :user)
+      visit account_team_path(@jane.current_team)
 
-    click_on "Add New Creative Concept"
-    fill_in "Name", with: "Test Name"
-    click_on "Create Creative Concept"
+      click_on "Add New Creative Concept"
+      fill_in "Name", with: "Test Name"
+      click_on "Create Creative Concept"
 
-    # Pagy::DEFAULT[:items] denotes the max of records that exist on one page.
-    (Pagy::DEFAULT[:items] + 1).times do |n|
-      click_on "Add New Tangible Thing"
-      fill_in "Text Field Value", with: "Test #{n + 1}"
-      click_on "Create Tangible Thing"
+      # Pagy::DEFAULT[:items] denotes the max of records that exist on one page.
+      (Pagy::DEFAULT[:items] + 1).times do |n|
+        click_on "Add New Tangible Thing"
+        fill_in "Text Field Value", with: "Test #{n + 1}"
+        click_on "Create Tangible Thing"
+      end
+
+      assert page.has_content?("Test 1")
+      refute page.has_content?("Test #{Pagy::DEFAULT[:items] + 1}")
+
+      click_on "Next"
+      assert page.has_content?("Test #{Pagy::DEFAULT[:items] + 1}")
     end
-
-    assert page.has_content?("Test 1")
-    refute page.has_content?("Test #{Pagy::DEFAULT[:items] + 1}")
-
-    click_on "Next"
-    assert page.has_content?("Test #{Pagy::DEFAULT[:items] + 1}")
   end
 end
