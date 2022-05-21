c1ef505ec3ba183d1bb038da2c73d6b175ded386
Andrew Culver <andrew.culver@gmail.com>
2022-05-21 10:22:40 -0700

    Skipping redundant test steps.

--- test/system/pagination_test.rb
+++ test/system/pagination_test.rb
@@ -10,25 +10,16 @@ class PaginationTest < ApplicationSystemTestCase
   test "pagination works properly" do
     display_details = @@test_devices[:macbook_pro_15_inch]
     resize_for(display_details)
-
     login_as(@jane, scope: :user)
-    visit account_team_path(@jane.current_team)
 
-    click_on "Add New Creative Concept"
-    fill_in "Name", with: "Test Name"
-    click_on "Create Creative Concept"
-
-    assert page.has_content? "Creative Concept was successfully created."
-
-    creative_concept = @team.scaffolding_absolutely_abstract_creative_concepts.order(:id).last
+    creative_concept = @team.scaffolding_absolutely_abstract_creative_concepts.create(name: "Test Name")
 
     # Pagy::DEFAULT[:items] denotes the max of records that exist on one page.
     (Pagy::DEFAULT[:items] + 1).times do |n|
       creative_concept.completely_concrete_tangible_things.create(text_field_value: "Test #{n + 1}")
     end
 
-    # Reload.
-    visit current_path
+    visit account_scaffolding_absolutely_abstract_creative_concept_path(creative_concept)
 
     assert page.has_content?("Test 1")
     refute page.has_content?("Test #{Pagy::DEFAULT[:items] + 1}")
