72557b5a8ce91fbd4d426a93e1920dc8c567d31a
Andrew Culver <andrew.culver@gmail.com>
2022-05-21 18:47:12 -0700

    Attempting to fix spring.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -13,8 +13,14 @@ Rails.application.configure do
 
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Turn false under Spring and add config.action_view.cache_template_loading = true
-  config.cache_classes = true
+  if ENV["SPRING"]
+    # We don't typically use Spring, but we use it for the Super Scaffolding test suite on CI.
+    config.cache_classes = false
+    config.action_view.cache_template_loading = true
+  else
+    # Turn false under Spring and add config.action_view.cache_template_loading = true
+    config.cache_classes = true
+  end
 
   # Eager loading loads your whole application. When running a single test locally,
   # this probably isn't necessary. It's a good idea to do in a continuous integration
