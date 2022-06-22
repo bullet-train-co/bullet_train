c5e8696517444f70a93fe14b290718422fd7b6b5
Gabriel Zayas <g-zayas@hotmail.com>
2022-06-22 15:46:44 +0900

    Minor adjustments to Super Scaffolding partial test

--- test/system/super_scaffolding_partial_test.rb
+++ test/system/super_scaffolding_partial_test.rb
@@ -63,22 +63,22 @@ class SuperScaffoldingSystemTest < ApplicationSystemTestCase
     test "super scaffolded color pickers function properly" do
       display_details = @@test_devices[:macbook_pro_15_inch]
       resize_for(display_details)
-
+  
       login_as(@jane, scope: :user)
       visit account_team_path(@jane.current_team)
-
-      assert page.has_content?("Add New ColorPicker")
-      click_on "Add New ColorPicker"
-
+  
+      assert page.has_content?("Add New Color Picker")
+      click_on "Add New Color Picker"
+  
       assert page.has_content?("Color Picker Value")
       color_picker_buttons = all(".button-color")
       assert_equal color_picker_buttons.size, 8
       color_picker_buttons.first.click
-      click_on "Create ColorPicker"
+      click_on "Create Color Picker"
+  
+      assert page.has_content?("Color Picker was successfully created.")
 
-      assert page.has_content?("ColorPicker was successfully created.")
-
-      # The default value can be found in the locale.
+      # The default value can be found in the color picker's locale.
       color_picker_default_value = "#9C73D2"
       assert_equal ColorPicker.first.color_picker_value, color_picker_default_value
       assert page.has_content?(color_picker_default_value)
