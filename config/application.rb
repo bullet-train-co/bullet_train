a366abce299cbe19131a1e58c3ba72b30f835981
Pascal Laliberté <pascal@hey.com>
2022-07-04 20:06:58 +0000

    rename application to Sharpen.page Availability

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 require_relative "../lib/bullet_train"
 
-module UntitledApplication
+module SharpenPageAvailability
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "untitled_application_production"
+  # config.active_job.queue_name_prefix = "sharpen_page_availability_production"
 
   config.action_mailer.perform_caching = false
 
