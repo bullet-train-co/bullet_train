76cb2151d0c4f1c4730cedea0120d855538c95d1
gazayas <g-zayas@hotmail.com>
2022-08-03 13:00:30 +0900

    Add tests for seeding? method

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 puts "🌱 Generating global seeds."
 
 load "#{Rails.root}/db/seeds/development.rb" if Rails.env.development?
+
+# We use this stub to test `seeding?` for ActiveRecord models.
+if Rails.env == "test" && ENV["seed_stub"] == "true"
+  User.create(email: "test@test.com", password: "956742469855eba772ea62b9f14d8626")
+  user = User.find_by(email: "test@test.com")
+  p "User is seeding: #{user.seeding?}"
+end
