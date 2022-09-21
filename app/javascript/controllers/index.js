6cf1f4f75c3bb55f6ac1379c857ab305656586e9
Pascal Laliberté <pascal@hey.com>
2022-09-21 20:34:15 +0000

    assign window.Stimulus for help debugging, even in production

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,6 +9,12 @@ import consumer from '../channels/consumer'
 
 const application = Application.start()
 
+// https://stimulus.hotwired.dev/handbook/installing#debugging
+// Use `window.Stimulus.debug = true` in the console to log actions and lifecycle hooks
+// on subsequent user interactions and Turbo page views.
+// Use `window.Stimulus.router.modulesByIdentifier` for a list of loaded controllers.
+window.Stimulus = application
+
 // Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js.
 import { context as controllersContext } from './**/*_controller.js';
