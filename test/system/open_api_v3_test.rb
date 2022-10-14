105993f92a260d90da3a8108cad75754c80df3c6
Andrew Culver <andrew.culver@gmail.com>
2022-10-14 15:07:08 -0700

    Fixing.

--- test/system/open_api_v3_test.rb
+++ test/system/open_api_v3_test.rb
@@ -2,7 +2,8 @@ require "application_system_test_case"
 
 class OpenApiV3Test < ApplicationSystemTestCase
   test "OpenAPI V3 document is valid" do
-    output = `yarn exec redocly lint http://localhost:3000/api/v1/openapi.yaml 1> /dev/stdout 2> /dev/stdout`
+    visit "http://localhost:3001/api/v1/openapi.yaml"
+    puts (output = `yarn exec redocly lint http://localhost:3001/api/v1/openapi.yaml 1> /dev/stdout 2> /dev/stdout; rm openapi.yaml`)
     assert output.include?("Woohoo! Your OpenAPI definition is valid.")
   end
 end
