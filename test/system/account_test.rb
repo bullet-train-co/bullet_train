c887e5c4bacce81480a85d3efd2be5fc93bee5b7
Andrew Culver <andrew.culver@gmail.com>
2022-12-15 14:38:35 -0800

    Fixing tests.

--- test/system/account_test.rb
+++ test/system/account_test.rb
@@ -56,7 +56,8 @@ class AccountTest < ApplicationSystemTestCase
       click_on "Next" if two_factor_authentication_enabled?
       fill_in "Your Password", with: another_example_password
       click_on "Sign In"
-      assert page.has_content?("Invalid Email Address or Password.")
+      # TODO I feel like password should be capitalized here?
+      assert page.has_content?("Invalid Email Address or password.")
     end
   end
 end
