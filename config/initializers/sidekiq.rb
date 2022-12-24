db83c7c6d6608209fbb3e5da3c5690b66b79511e
gazayas <g-zayas@hotmail.com>
2022-12-24 14:47:25 +0900

    Add Sidekiq initializer

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,7 @@
+Sidekiq.configure_server do |config|
+  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+end
