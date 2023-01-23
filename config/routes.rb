71c4508a369cf8a761dcc3b62af1d88bb87742cf
Peter Baumgartner <pete@lincolnloop.com>
2023-01-23 11:47:32 -0700

    Add basic healthcheck endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/healthcheck', to: proc { [200, {}, ["ok"]] }
   # See `config/routes/*.rb` to customize these configurations.
   draw "concerns"
   draw "devise"
