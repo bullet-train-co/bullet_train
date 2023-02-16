b83f31ce77410a37d67fed4787147afc6acac36e
Pascal Laliberté <pascal@hey.com>
2023-02-16 02:07:05 +0000

    tests: fix find_stimulus_controller_for_label to permit multiple controllers

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -259,9 +259,9 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   def find_stimulus_controller_for_label(label, stimulus_controller, wrapper = false)
     if wrapper
       wrapper_el = find("label", text: /\A#{label}\z/).first(:xpath, ".//..//..")
-      wrapper_el if wrapper_el["data-controller"] == stimulus_controller
+      wrapper_el if wrapper_el["data-controller"].split(" ").include?(stimulus_controller)
     else
-      find("label", text: /\A#{label}\z/).first(:xpath, ".//..").first('[data-controller="' + stimulus_controller + '"]')
+      find("label", text: /\A#{label}\z/).first(:xpath, ".//..").first('[data-controller~="' + stimulus_controller + '"]')
     end
   end
 
