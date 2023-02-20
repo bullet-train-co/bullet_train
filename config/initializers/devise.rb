6c3d980dfffc5874ac2bb55bcd9f2ccb45474512
Gabriel Zayas <g-zayas@hotmail.com>
2023-02-20 20:46:22 +0900

    English

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  config.responder.error_status = :unprocessible_entity
+  config.responder.error_status = :unprocessable_entity
   config.responder.redirect_status = :see_other
 
   config.omniauth :stripe_connect, ENV["STRIPE_CLIENT_ID"], ENV["STRIPE_SECRET_KEY"], scope: "read_write"
