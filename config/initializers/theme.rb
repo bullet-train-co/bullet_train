ed0ec17454170e55ecafe039fa9bb3ab49dcb05a
Gabriel Zayas <g-zayas@hotmail.com>
2023-03-23 16:59:16 +0900

    Add more configuration options to theme initializer

--- config/initializers/theme.rb
+++ config/initializers/theme.rb
@@ -1,1 +1,10 @@
-BulletTrain::Themes::Light.color = :blue
+# Bullet Train theme configuration.
+
+# Main color
+BulletTrain::Themes::Light.color = :slate
+
+# Navbar orientation
+# BulletTrain::Themes::Light.navigation = :left
+#
+# Logo
+# BulletTrain::Themes::Light.show_logo_in_account = true
