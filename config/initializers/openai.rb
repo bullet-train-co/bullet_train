50f407951e9894838cbb45df114fdf449b4c4960
Gabriel Zayas <g-zayas@hotmail.com>
2023-03-24 22:06:53 +0900

    Fix spacing

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,6 +1,6 @@
 if openai_enabled?
   OpenAI.configure do |config|
-    config.access_token = ENV["OPENAI_ACCESS_TOKEN"] 
+    config.access_token = ENV["OPENAI_ACCESS_TOKEN"]
     config.organization_id = ENV["OPENAI_ORGANIZATION_ID"] if openai_organization_exists?
 
     # Set request timeout. Default is 120 seconds.
