ae96c94b0803b7e3651f9f06987fccd57afe00cd
Yuri Sidorov <403994+newstler@users.noreply.github.com>
2023-04-08 00:11:39 +0200

    Fix: uninitialized constant FactoryBot::Syntax on Heroku

--- config/initializers/factory_bot.rb
+++ config/initializers/factory_bot.rb
@@ -1,7 +1,7 @@
-if defined?(FactoryBot)
+# frozen_string_literal: true
 
+if Rails.env.test? && defined?(FactoryBot)
   class ActiveSupport::TestCase
     include FactoryBot::Syntax::Methods
   end
-
 end
