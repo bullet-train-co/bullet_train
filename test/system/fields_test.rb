cf3a2e9535aca5d147544fa9cc18acb2853d181e
Gabriel Zayas <g-zayas@hotmail.com>
2023-05-16 20:08:35 +0900

    Add TODO for fields system test

--- test/system/fields_test.rb
+++ test/system/fields_test.rb
@@ -6,10 +6,15 @@ unless scaffolding_things_disabled?
       test "simulate restoring behavior of form fields on page restore on #{device_name}" do
         resize_for(display_details)
 
-        # Ensure no one is logged in when running the entire test suite.
-        sign_out_for(display_details)
-
         visit root_path
+
+        # TODO: This is an ugly hack to ensure the default test user isn't logged in.
+        # This is happening when billing is enabled and the entire test suite is run.
+        if page.text.match?("Jane Smith")
+          puts "Running here."
+          sign_out_for(display_details) 
+          visit root_path
+        end
 
         invitation_only? ? be_invited_to_sign_up : click_on("Don't have an account?")
         assert page.has_content?("Create Your Account")
