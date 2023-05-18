c199bc0f0893c5243fe2afdf2e6426604b5b3fb1
Adrian <adrian@adrianthedev.com>
2023-05-19 01:56:02 +0300

    feature: add Avo admin panel

--- app/avo/resources/membership_resource.rb
+++ app/avo/resources/membership_resource.rb
@@ -1,28 +1,18 @@
 class MembershipResource < Avo::BaseResource
-  self.title = :id
+  self.title = :name
   self.includes = []
   # self.search_query = -> do
   #   scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
   # end
 
   field :id, as: :id
-  # Fields generated from the model
-  field :user_id, as: :number
-  field :team_id, as: :number
-  field :invitation_id, as: :number
-  field :user_first_name, as: :text
-  field :user_last_name, as: :text
+  field :name, as: :text, hide_on: :forms
   field :user_profile_photo_id, as: :text
   field :user_email, as: :text
   field :added_by_id, as: :number
-  field :platform_agent_of_id, as: :number
-  field :role_ids, as: :text
-  field :platform_agent, as: :boolean
-  field :webhooks_outgoing_events, as: :has_many
+  field :role_ids, as: :tags
   field :user, as: :belongs_to
   field :team, as: :belongs_to
   field :invitation, as: :belongs_to
   field :added_by, as: :belongs_to
-  field :platform_agent_of, as: :belongs_to
-  # add fields here
 end
