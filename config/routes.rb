c5cf95f954440283f7799fbb35c395664bbe9e11
Gabriel Zayas <g-zayas@hotmail.com>
2023-08-10 10:01:53 +0900

    Move collection_actions into RoutesHelper

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,6 @@ Rails.application.routes.draw do
   draw "devise"
   draw "sidekiq"
 
-  # This is helpful to have around when working with shallow routes and complicated model namespacing. We don't use this
-  # by default, but sometimes Super Scaffolding will generate routes that use this for `only` and `except` options.
-  # TODO Would love to get this out of the application routes file.
-  collection_actions = [:index, :new, :create] # standard:disable Lint/UselessAssignment
-
   # This helps mark `resources` definitions below as not actually defining the routes for a given resource, but just
   # making it possible for developers to extend definitions that are already defined by the `bullet_train` Ruby gem.
   # TODO Would love to get this out of the application routes file.
