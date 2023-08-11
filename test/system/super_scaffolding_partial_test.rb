94454bd13f10792bf3091be720cf8b405b4299e7
Samuel BOHN <bohn.sjl@gmail.com>
2023-08-11 19:19:37 +0200

    update tests in super_scaffolding_partial_test for localized fields

--- test/system/super_scaffolding_partial_test.rb
+++ test/system/super_scaffolding_partial_test.rb
@@ -113,10 +113,10 @@ class SuperScaffoldingSystemTest < ApplicationSystemTestCase
       find("#partial_test_multiple_buttons_test_two+button", visible: :all).click
       find("#partial_test_multiple_buttons_test_three+button", visible: :all).click
       # Date partial
-      find("#partial_test_date_test").click
+      find("#partial_test_date_test_display").click
       find(".daterangepicker").click_on("Apply") # Chooses today's date.
       # DateTime partial
-      find("#partial_test_date_time_test").click
+      find("#partial_test_date_time_test_display").click
       find(".daterangepicker").click_on("Apply")
       # File partial
       attach_file("test/support/foo.txt", make_visible: true)
