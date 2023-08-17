408a8fd89ad3cb45385d42a28a86c5598b2aa4da
Adrian <adrian@adrianthedev.com>
2023-08-18 01:49:27 +0300

    remove the dashboard

--- app/avo/cards/users_count.rb
+++
@@ -1,9 +0,0 @@
-class Avo::Cards::UsersCount < Avo::Dashboards::MetricCard
-  self.id = "users_count"
-  self.label = "Users count"
-  self.description = "Count of registered users"
-
-  def query
-    result ::User.all.count
-  end
-end
