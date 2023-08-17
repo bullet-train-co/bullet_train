ccbcdb0efbc420621b1d0ce44d34c5df6c7a7786
Adrian <adrian@adrianthedev.com>
2023-08-18 01:43:30 +0300

    wip

--- app/avo/cards/users_count.rb
+++ app/avo/cards/users_count.rb
@@ -1,7 +1,7 @@
 class Avo::Cards::UsersCount < Avo::Dashboards::MetricCard
   self.id = "users_count"
   self.label = "Users count"
-  self.description = "Total number of registered users"
+  self.description = "Count of registered users"
 
   def query
     result ::User.all.count
