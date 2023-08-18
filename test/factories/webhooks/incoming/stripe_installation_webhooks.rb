61393f952f73b3932e78fdc2f4bd2d4b9814f1e1
Jeremy Green <jagthedrummer@gmail.com>
2023-08-18 12:44:06 -0700

    Add tests for Stripe webhooks (#881)
    This implements some tests for bullet-train-pro/bullet_train-billing-stripe#18. If the bullet_train-billing-stripe gem is not installed then these new tests will be skipped.
    
    It also removes a (seemingly unused) factory that was duplicated out of bullet_train-billing-stripe.
    
    This app still has access to that factory via the changes made to test_helper.rb.

--- test/factories/webhooks/incoming/stripe_installation_webhooks.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :webhooks_incoming_stripe_installation_webhook, class: "Webhooks::Incoming::StripeInstallationWebhook" do
-    data { "" }
-    processed_at { "2021-03-16 18:04:09" }
-    verified_at { "2021-03-16 18:04:09" }
-    integrations_stripe_installation { nil }
-  end
-end
