421fea7186b1343b5a81c315a97824b99d26f4a0
Jeremy Green <jeremy@octolabs.com>
2023-08-18 11:51:08 -0500

    Tests for stripe webhook handling

---
+++ test/models/webhooks/incoming/stripe_webhook_test.rb
@@ -0,0 +1,116 @@
+require 'test_helper'
+
+if defined?(BulletTrain::Billing::Stripe)
+  class Webhooks::Incoming::StripeWebhookTest < ActiveSupport::TestCase
+    include ::FactoryBot::Syntax::Methods
+    # TODO: Cut this down to just the attributes we need
+    def customer_subscription_updated_data(created_at, stripe_subscription_id)
+      {
+        "id" => "evt_1NgIq2BiWqhbiHpFXvtRJZtY",
+        "data" => {
+          "object" => {
+            "id" => stripe_subscription_id,
+            "plan" => {
+            },
+            "items" => {
+              "data" => [],
+            },
+            "object" => "subscription",
+            "status" => "active",
+            "created" => created_at.to_i,
+            "cancel_at_period_end" => false,
+            "cancellation_details" => {
+              "reason" => nil, "comment" => nil, "feedback" => nil
+            },
+          }
+        },
+        "type" => "customer.subscription.updated",
+        "object" => "event",
+        "created" => created_at.to_i,
+        "request" => {
+          "id" => "req_xecKk5kzBB6YxH", "idempotency_key" => "4b0d30d5-4bbd-400b-87ae-f551e3be0748"
+        },
+        "livemode" => true,
+        "api_version" => "2022-11-15",
+        "pending_webhooks" => 2
+      }
+    end
+
+    # TODO: Cut this down to just the attributes we need
+    def customer_subscription_created_data(created_at, stripe_subscription_id)
+      {
+        "id" => "evt_1NgIq1BiWqhbiHpFTeqASYDz",
+        "data" => {
+          "object" => {
+            "id" => stripe_subscription_id,
+            "plan" => {
+            },
+            "items" => {
+              "data" => [],
+            },
+            "object" => "subscription",
+            "status" => "incomplete",
+            "created" => created_at.to_i,
+            "cancel_at_period_end" => false,
+            "cancellation_details" => {
+              "reason" => nil, "comment" => nil, "feedback" => nil
+            },
+          }
+        },
+        "type" => "customer.subscription.created",
+        "object" => "event",
+        "created" => created_at.to_i,
+        "request" => {
+          "id" => "req_xecKk5kzBB6YxH", "idempotency_key" => "4b0d30d5-4bbd-400b-87ae-f551e3be0748"
+        },
+        "livemode" => true,
+        "api_version" => "2022-11-15",
+        "pending_webhooks" => 2
+      }
+    end
+
+    class SubscriptionValidOrderTests < Webhooks::Incoming::StripeWebhookTest
+      def setup
+        team = create :team
+        stripe_subscription = create :billing_stripe_subscription, team: team
+        puts stripe_subscription.as_json
+        @generic_subscription = create :billing_subscription, provider_subscription: stripe_subscription, status: nil
+        #create :billing_subscriptions_included_price, subscription: @generic_subscription
+        @subscription_created_webhook = create :webhooks_incoming_stripe_webhook,
+          data: customer_subscription_created_data(Time.now - 2.minutes, stripe_subscription.stripe_subscription_id)
+        @subscription_updated_webhook = create :webhooks_incoming_stripe_webhook,
+          data: customer_subscription_updated_data(Time.now - 1.minutes, stripe_subscription.stripe_subscription_id)
+      end
+
+      test "the subscription ends up as 'active'" do
+        @subscription_created_webhook.process
+        assert_equal "pending", @generic_subscription.reload.status
+        @subscription_updated_webhook.process
+        assert_equal "active", @generic_subscription.reload.status
+      end
+    end
+
+    class SubscriptionInvalidOrderTests < Webhooks::Incoming::StripeWebhookTest
+      def setup
+        team = create :team
+        stripe_subscription = create :billing_stripe_subscription, team: team
+        puts stripe_subscription.as_json
+        @generic_subscription = create :billing_subscription, provider_subscription: stripe_subscription, status: nil
+        #create :billing_subscriptions_included_price, subscription: @generic_subscription
+        @subscription_updated_webhook = create :webhooks_incoming_stripe_webhook,
+          data: customer_subscription_updated_data(Time.now - 2.minutes, stripe_subscription.stripe_subscription_id)
+        @subscription_created_webhook = create :webhooks_incoming_stripe_webhook,
+          data: customer_subscription_created_data(Time.now - 1.minutes, stripe_subscription.stripe_subscription_id)
+      end
+
+      test "the subscription ends up as 'active'" do
+        @subscription_updated_webhook.process
+        assert_equal "active", @generic_subscription.reload.status
+        @subscription_created_webhook.process
+        assert_equal "active", @generic_subscription.reload.status
+      end
+    end
+
+
+  end
+end
