cf87c1e99b4a8eb0af473f5396933c1ae5c84cfb
Yuri Sidorov <403994+newstler@users.noreply.github.com>
2023-08-22 21:30:22 +0200

    Memberships API (#725)

---
+++ app/views/api/v1/memberships/_membership.json.jbuilder
@@ -0,0 +1,16 @@
+json.extract! membership,
+  :id,
+  :user_id,
+  :team_id,
+  :invitation_id,
+  :user_first_name,
+  :user_last_name,
+  :user_profile_photo_id,
+  :user_email,
+  :added_by_id,
+  :platform_agent_of_id,
+  :role_ids,
+  :platform_agent,
+  # 🚅 super scaffolding will insert new fields above this line.
+  :created_at,
+  :updated_at
--- test/factories/memberships.rb
+++ test/factories/memberships.rb
@@ -2,5 +2,19 @@ FactoryBot.define do
   factory :membership do
     association :user
     association :team
+
+    factory :membership_example do
+      team { FactoryBot.example(:team) }
+      user { FactoryBot.example(:user, teams: [team]) }
+      invitation_id { nil }
+      user_first_name { user.first_name }
+      user_last_name { user.last_name }
+      user_profile_photo_id { 1 }
+      user_email { user.email }
+      added_by_id { FactoryBot.example(:user).id }
+      platform_agent_of_id { nil }
+      platform_agent { false }
+      role_ids { ["admin"] }
+    end
   end
 end
