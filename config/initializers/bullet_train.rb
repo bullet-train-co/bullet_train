70d72cd47c258d61661ae58b6369977f5541b1db
gazayas <g-zayas@hotmail.com>
2023-08-23 21:40:12 +0900

    Add comment in BT initializer for bulk invitations

--- config/initializers/bullet_train.rb
+++ config/initializers/bullet_train.rb
@@ -1,4 +1,7 @@
 BulletTrain.configure do |config|
   # Uncomment this line if you want to bypass strong passwords.
   # config.strong_passwords = false
+
+  # Uncomment this line to enable bulk invitations on the user onboarding step.
+  # config.enable_bulk_invitations = true
 end
