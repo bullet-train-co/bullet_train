06acec10b661b50bdb33fbb83e94885e434abac0
gazayas <g-zayas@hotmail.com>
2023-08-23 20:57:59 +0900

    Add configuration for incoming webhook parent class

--- config/initializers/bullet_train.rb
+++ config/initializers/bullet_train.rb
@@ -1,4 +1,7 @@
 BulletTrain.configure do |config|
   # Uncomment this line if you want to bypass strong passwords.
   # config.strong_passwords = false
+
+  # Change the parent class you want incoming webhooks to inherit from.
+  config.incoming_webhooks_parent_class_name = "ApplicationRecord"
 end
