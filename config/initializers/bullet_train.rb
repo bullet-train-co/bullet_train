bc794d5295d552406eb7cec7812568094e11bde8
gazayas <g-zayas@hotmail.com>
2023-08-23 22:11:11 +0900

    Enable bulk invitations on bin/configure step

--- config/initializers/bullet_train.rb
+++ config/initializers/bullet_train.rb
@@ -2,6 +2,6 @@ BulletTrain.configure do |config|
   # Uncomment this line if you want to bypass strong passwords.
   # config.strong_passwords = false
 
-  # Uncomment this line to enable bulk invitations on the user onboarding step.
+  # Enable bulk invitations on the user onboarding step.
   # config.enable_bulk_invitations = true
 end
