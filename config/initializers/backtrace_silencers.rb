05bc8acc9ddbb58753b180bf2d67b561b9042512
Jeremy Green <jeremy@octolabs.com>
2023-08-28 15:42:30 -0500

    Show BulletTrain gems in stack traces by default
    Fixes https://github.com/bullet-train-co/bullet_train/issues/681

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -3,6 +3,16 @@
 # You can add backtrace silencers for libraries that you're using but don't wish to see in your backtraces.
 # Rails.backtrace_cleaner.add_silencer { |line| /my_noisy_library/.match?(line) }
 
+
+
 # You can also remove all the silencers if you're trying to debug a problem that might stem from framework code
 # by setting BACKTRACE=1 before calling your invocation, like "BACKTRACE=1 ./bin/rails runner 'MyClass.perform'".
-Rails.backtrace_cleaner.remove_silencers! if ENV["BACKTRACE"]
+Rails.backtrace_cleaner.remove_silencers!
+if !ENV["BACKTRACE"]
+  # If the user hasn't acked to see the FULL backtrace, we silece everything that rails silences by default,
+  # except for lines that look like they come from a BulletTrain gem.
+  Rails.backtrace_cleaner.add_silencer do |line|
+    (line !~ Rails::BacktraceCleaner::APP_DIRS_PATTERN) &&
+      (line !~ /^bullet_train/  )
+  end
+end
