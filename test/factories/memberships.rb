7cef9e36d72d5a7e3c380c0442f96c3c51306231
Jeremy Green <jeremy@octolabs.com>
2023-08-28 12:14:50 -0500

    Fix a flaky test

--- test/factories/memberships.rb
+++ test/factories/memberships.rb
@@ -11,7 +11,16 @@ FactoryBot.define do
       user_last_name { user.last_name }
       user_profile_photo_id { 1 }
       user_email { user.email }
-      added_by_id { FactoryBot.example(:user).id }
+      # TODO: If we try to generate an `added_by` relationship in the factory we get stuck in an
+      # infinite loop that eventually results in the following error:
+      # Minitest::UnexpectedError: SystemStackError: stack level too deep
+      #
+      # Is there some way we can hadle this in the factory?
+      # And/or do we _need_ to? Maybe we can just remove this?
+      # Note: This used to incorrectly generate a :user instead of a :membership, but
+      # added_by_id is indexed to the `memberships` table, so generating a :user is not valid.
+      #
+      # added_by_id { FactoryBot.example(:membership).id }
       platform_agent_of_id { nil }
       platform_agent { false }
       role_ids { ["admin"] }
