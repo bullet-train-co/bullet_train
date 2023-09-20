1c120deaf2eb83041cb8f160fafa4c6efe620228
Jeremy Green <jagthedrummer@gmail.com>
2023-09-20 09:35:32 -0700

    Implement mailer previews for built-in emails (#1013)
    * Mailer previews for built-in emails
    
    Fixes https://github.com/bullet-train-co/bullet_train-core/issues/266
    
    * Previews for Devise mails
    
    * linter

---
+++ test/mailers/previews/devise_mailer_preview.rb
@@ -0,0 +1,23 @@
+class DeviseMailerPreview < ActionMailer::Preview
+  # TODO: We probably want to turn on the :confirmable feature
+  # def confirmation_instructions
+  #   Devise::Mailer.confirmation_instructions(User.first, "faketoken")
+  # end
+
+  def reset_password_instructions
+    Devise::Mailer.reset_password_instructions(User.first, "faketoken")
+  end
+
+  # TODO: Do we want to turn on the :lockable feature?
+  # def unlock_instructions
+  #   Devise::Mailer.unlock_instructions(User.first, "faketoken")
+  # end
+
+  def email_changed
+    Devise::Mailer.email_changed(User.first)
+  end
+
+  def password_changed
+    Devise::Mailer.password_change(User.first)
+  end
+end
