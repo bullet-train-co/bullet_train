0a9636a822e9ae2e61f6861c4c6122269939eb67
Jeremy Green <jeremy@octolabs.com>
2023-09-20 11:24:32 -0500

    linter

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,11 +1,11 @@
 class UserMailerPreview < ActionMailer::Preview
   def welcome
-    random_user = User.order(Arel.sql('RANDOM()')).first
+    random_user = User.order(Arel.sql("RANDOM()")).first
     UserMailer.welcome random_user
   end
 
   def invited
-    random_invitation = Invitation.order(Arel.sql('RANDOM()')).first
+    random_invitation = Invitation.order(Arel.sql("RANDOM()")).first
     UserMailer.invited random_invitation.uuid
   end
 end
