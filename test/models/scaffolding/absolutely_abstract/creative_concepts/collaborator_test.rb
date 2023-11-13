cc5e87328ed4da2b31354e258ee8a2161b27ddfc
Gabriel Zayas <g-zayas@hotmail.com>
2023-11-14 01:01:27 +0900

    Remove collaborators example (#1142)
    * Remove collaborators example
    
    * Remove Collborator resource from roles

--- test/models/scaffolding/absolutely_abstract/creative_concepts/collaborator_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class Scaffolding::AbsolutelyAbstract::CreativeConcepts::CollaboratorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
