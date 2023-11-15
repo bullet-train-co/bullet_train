5817d2d2985b5df3a26c8449eab5fd57b0bb3d5b
Gabriel Zayas <g-zayas@hotmail.com>
2023-11-16 01:14:01 +0900

    Run Collaborator related migrations only when the model is present (#1154)
    * Only update roles column if the collaborators example is available
    
    * Use correct model name in migration

--- db/migrate/20211130142547_rename_roles_to_role_ids.rb
+++ db/migrate/20211130142547_rename_roles_to_role_ids.rb
@@ -1,6 +1,8 @@
 class RenameRolesToRoleIds < ActiveRecord::Migration[6.1]
   def change
-    rename_column :scaffolding_absolutely_abstract_creative_concepts_collaborators, :roles, :role_ids
-    Scaffolding::AbsolutelyAbstract::CreativeConcepts::Collaborator.update_all role_ids: []
+    if defined?(Scaffolding::AbsolutelyAbstract::CreativeConcepts::Collaborator)
+      rename_column :scaffolding_absolutely_abstract_creative_concepts_collaborators, :roles, :role_ids
+      Scaffolding::AbsolutelyAbstract::CreativeConcepts::Collaborator.update_all role_ids: []
+    end
   end
 end
