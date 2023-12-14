65516b811b9db61adc1ee27e08d20b95853c25c1
Jeremy Green <jeremy@octolabs.com>
2023-12-14 11:47:45 -0600

    dang linter

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -31,13 +31,13 @@ else # Selenium
     version = Capybara::Selenium::Driver.load_selenium
     options_key = Capybara::Selenium::Driver::CAPS_VERSION.satisfied_by?(version) ? :capabilities : :options
     browser_options = Selenium::WebDriver::Chrome::Options.new.tap do |opts|
-      opts.add_argument('--headless=new')
-      opts.add_argument('--disable-gpu') if Gem.win_platform?
+      opts.add_argument("--headless=new")
+      opts.add_argument("--disable-gpu") if Gem.win_platform?
       # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
-      opts.add_argument('--disable-site-isolation-trials')
+      opts.add_argument("--disable-site-isolation-trials")
     end
 
-    Capybara::Selenium::Driver.new(app, **{ :browser => :chrome, options_key => browser_options })
+    Capybara::Selenium::Driver.new(app, **{:browser => :chrome, options_key => browser_options})
   end
 
   Capybara.javascript_driver = ENV["MAGIC_TEST"].present? ? :selenium_chrome : :selenium_chrome_headless
