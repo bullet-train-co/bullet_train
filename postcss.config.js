32318df5cf26419c77147695502100ac92de97d3
Pascal Laliberté <pascal@hey.com>
2023-12-19 13:05:28 -0500

    postcss-import: if found, replace  path alias

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,19 @@
+const path = require('path');
+const { execSync } = require("child_process");
+
+const themeRoot = execSync(`bundle show bullet_train-themes-${process.env.THEME} 2> /dev/null`).toString().trim()
+const themeStylesheets = path.resolve(themeRoot, 'app/assets/stylesheets/');
+
 module.exports = {
   plugins: [
-    require('postcss-import'),
+    require('postcss-import')({
+      resolve: (id, basedir, importOptions) => {
+        if (id.startsWith('$THEMESTYLESHEETS')) {
+          id = id.replace('$THEMESTYLESHEETS', themeStylesheets);
+        }
+        return id;
+      }
+    }),
     require('postcss-extend-rule'),
     require('postcss-nested'),
     require('tailwindcss'),
