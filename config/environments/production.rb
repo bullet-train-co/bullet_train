ba439f7534614b4c9cf2cb826b3e3987e5c97abf
Julian Rubisch <julian@julianrubisch.at>
2024-01-02 18:34:21 +0100

    feat: Add terser for js compression in production (#1249)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,9 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
+  # Compress JavaScript
+  config.assets.js_compressor = :terser
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
