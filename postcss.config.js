8c9c47dc06f19e0d712a583e8f629984dbe99386
Pascal Laliberté <pascal@hey.com>
2024-01-11 11:27:16 -0500

    resolve $ThemeStylesheet, rename $ThemeStylesheetsDir alias

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,20 @@
 const path = require('path');
 const { execSync } = require("child_process");
 
+const themeStylesheet = execSync(`bundle exec bin/theme tailwind-stylesheet ${process.env.THEME} 2> /dev/null`).toString().trim()
 const themeRoot = execSync(`bundle show bullet_train-themes-${process.env.THEME} 2> /dev/null`).toString().trim()
-const themeStylesheets = path.resolve(themeRoot, 'app/assets/stylesheets/');
+const themeStylesheetsDir = path.resolve(themeRoot, 'app/assets/stylesheets/');
 
 module.exports = {
   plugins: [
     require('postcss-import')({
       resolve: (id, basedir, importOptions) => {
-        if (id.startsWith('$THEMESTYLESHEETS')) {
-          id = id.replace('$THEMESTYLESHEETS', themeStylesheets);
+        if (id.startsWith('$ThemeStylesheetsDir')) {
+          initial_id = id;
+          id = id.replace('$ThemeStylesheetsDir', themeStylesheetsDir);
+          console.log(initial_id, "$ThemeStylesheetsDir", themeStylesheetsDir, id)
+        } else if (id.startsWith('$ThemeStylesheet')) {
+          id = id.replace('$ThemeStylesheet', themeStylesheet);
         }
         return id;
       }
