b7e12c9bcf80c204993f94438e295ece23da26e8
Pascal Laliberté <pascal@hey.com>
2024-01-11 13:38:28 -0500

    extract postcss-import-config, apply to postcss.mailer

--- postcss.config.js
+++ postcss.config.js
@@ -1,24 +1,11 @@
-const path = require('path');
 const { execSync } = require("child_process");
 
-const themeStylesheet = execSync(`bundle exec bin/theme tailwind-stylesheet ${process.env.THEME} 2> /dev/null`).toString().trim()
-const themeRoot = execSync(`bundle show bullet_train-themes-${process.env.THEME} 2> /dev/null`).toString().trim()
-const themeStylesheetsDir = path.resolve(themeRoot, 'app/assets/stylesheets/');
+const postcssImportConfigFile = execSync(`bundle exec bin/theme postcss-import-config ${process.env.THEME}`).toString().trim()
+const postcssImportConfig = require(postcssImportConfigFile)
 
 module.exports = {
   plugins: [
-    require('postcss-import')({
-      resolve: (id, basedir, importOptions) => {
-        if (id.startsWith('$ThemeStylesheetsDir')) {
-          initial_id = id;
-          id = id.replace('$ThemeStylesheetsDir', themeStylesheetsDir);
-          console.log(initial_id, "$ThemeStylesheetsDir", themeStylesheetsDir, id)
-        } else if (id.startsWith('$ThemeStylesheet')) {
-          id = id.replace('$ThemeStylesheet', themeStylesheet);
-        }
-        return id;
-      }
-    }),
+    require('postcss-import')(postcssImportConfig),
     require('postcss-extend-rule'),
     require('postcss-nested'),
     require('tailwindcss'),
--- postcss.mailer.config.js
+++ postcss.mailer.config.js
@@ -1,6 +1,11 @@
+const { execSync } = require("child_process");
+
+const postcssImportConfigFile = execSync(`bundle exec bin/theme postcss-import-config ${process.env.THEME}`).toString().trim()
+const postcssImportConfig = require(postcssImportConfigFile)
+
 module.exports = {
   plugins: [
-    require('postcss-import'),
+    require('postcss-import')(postcssImportConfig),
     require('postcss-extend-rule'),
     require('postcss-nested'),
     require('tailwindcss'),
