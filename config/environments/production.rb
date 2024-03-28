1e0bf6da25082b3225feb7ad3723b2b61a99e8d1
Jeremy Green <jeremy@octolabs.com>
2024-03-28 09:02:50 -0500

    Revert "BT setup"
    This reverts commit 0f740e8460b32ba850e9652b0e086cd8d0140897.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "np_tester_production"
+  # config.active_job.queue_name_prefix = "untitled_application_production"
 
   config.action_mailer.perform_caching = false
 
