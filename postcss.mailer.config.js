0c5b3b4802cc3f863d9d73ccccec42e35467933b
Jeremy Green <jeremy@octolabs.com>
2024-04-05 14:11:18 -0500

    Add a link to a report about variable support in email clients

--- postcss.mailer.config.js
+++ postcss.mailer.config.js
@@ -7,6 +7,9 @@ module.exports = {
   plugins: [
     require('postcss-import')(postcssImportConfig),
     require('postcss-extend-rule'),
+    // CSS variables aren't currently well supported in email clients.
+    // https://www.caniemail.com/search/?s=variables
+    // Maybe someday we can remove this next plugin for the mailer stylesheet.
     require('postcss-css-variables'),
     require('tailwindcss/nesting'),
     require('tailwindcss'),
