9beec69d0483ad45e4f233774cfa16210202dcb8
Jeremy Green <jeremy@octolabs.com>
2024-05-21 14:45:11 -0500

    Commit changes after updating configs

--- bin/configure-scripts/update_configs.rb
+++ bin/configure-scripts/update_configs.rb
@@ -80,3 +80,12 @@ puts `rm .github/FUNDING.yml`.chomp
 # File.write("config/initializers/bullet_train.rb", bt_config_lines.join)
 
 
+commit_changes = ask_boolean "Would you like to commit the changes to your project?", "y"
+if commit_changes
+  puts "Committing all these changes to the repository.".green
+  stream "git add -A"
+  stream "git commit -m \"Run configuration script.\""
+else
+  puts ""
+  puts "Make sure you save your changes with Git.".yellow
+end
