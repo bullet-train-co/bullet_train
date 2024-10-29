e9c76dbe8110370477473ec2278d4a5058e6878f
Jeremy Green <jeremy@octolabs.com>
2024-10-29 13:38:29 -0500

    got these working from the core repo

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,15 +1,3 @@
 class Api::V1::ApplicationController < ActionController::API
   include Api::Controllers::Base
-
-  #def reorder
-    #raise "dummy reorder action on api controller"
-  #end
-
-  #def new
-    #raise "dummy new action on api controller"
-  #end
-
-  #def edit
-    #raise "dummy edit action on api controller"
-  #end
 end
