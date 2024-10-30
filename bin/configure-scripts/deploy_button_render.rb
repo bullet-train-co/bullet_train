61145c0e053ab3d33bd4f5852d2a99f4521bc480
Jeremy Green <jagthedrummer@gmail.com>
2024-10-30 11:26:30 -0500

    Revert "Remove addition of deploy buttons from `bin/configure` (#1727)" (#1728)
    This reverts commit 9df62a6fff6bd8032ab71ef4c26f699b1e433c10.

---
+++ bin/configure-scripts/deploy_button_render.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+require "#{__dir__}/utils"
+
+announce_section "Add a 'Deploy to Render' button?"
+
+button_file = "#{__dir__}/deploy-buttons/render.md"
+
+add_button = ask_boolean "Would you like to add a 'Deploy to Render' button to your project.", "y"
+if add_button
+  puts "Adding a 'Deploy to Render' button.".green
+  new_repo_link = ask "What is the https variant of your repo URL? (Something like: https://github.com/your-org/your-repo)"
+
+  File.open("README.md", "a") do |readme|
+    button = File.read(button_file)
+    button.each_line do |line|
+      readme << line
+    end
+  end
+
+  original_repo_link = "https://github.com/bullet-train-co/bullet_train"
+
+  replace_in_file("README.md", original_repo_link, new_repo_link, /repo=#{original_repo_link}/)
+else
+  puts "Not adding a 'Deploy to Render' button.".yellow
+end
