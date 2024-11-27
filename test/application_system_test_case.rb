ecf9605727d162bf51fb593a7587f764da6886ce
Jeremy Green <jeremy@octolabs.com>
2024-11-27 14:41:05 -0600

    mostly working...

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -35,9 +35,13 @@ Capybara.register_server :puma do |app, port, host|
   Rack::Handler::Puma.run(app, Host: host, Port: port, Threads: "5:5", config_files: ["-"])
 end
 
+puts "--------------------------------"
+puts ENV['TEST_ENV_NUMBER']
+puts "--------------------------------"
+
 Capybara.server = :puma
-Capybara.server_port = 3001
-Capybara.app_host = "http://localhost:3001"
+Capybara.server_port = 3001  + ENV['TEST_ENV_NUMBER'].to_i
+Capybara.app_host = "http://localhost:#{Capybara.server_port}"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   def self.use_cuprite?
@@ -76,7 +80,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   end
 
   setup do
-    ENV["BASE_URL"] = "http://localhost:3001"
+    ENV["BASE_URL"] = "http://localhost:3001#{ENV['TEST_ENV_NUMBER'].to_i}"
     Capybara.use_default_driver
     Capybara.reset_sessions!
   end
