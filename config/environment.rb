25ad011126438d065b456d957f8c242dc155deeb
Jeremy Green <jeremy@octolabs.com>
2024-12-09 13:11:31 -0600

    that seems to prevent it from reporting sometimes

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,12 @@
 if ENV["CAPTURE_SIMPLECOV_AT_RUNTIME"]
+  puts "------------------------------------"
+  puts "Enabling simplecov"
+  puts "------------------------------------"
   require "simplecov"
   # We use a random component here so that when we call `rails g super_scaffold` multiple times
   # we merge that coverage info instead of clobbering it.
   SimpleCov.command_name "runtime-simplecov-#{SecureRandom.hex}"
-  SimpleCov.external_at_exit = true
   SimpleCov.start "rails"
-  # Rails.application.eager_load!
 end
 
 # Load the Rails application.
