37e0d2dc9d6542bd411a47850f4dc84f40b5fe65
Jeremy Green <jeremy@octolabs.com>
2024-12-09 10:55:41 -0600

    does this help

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,9 @@ if ENV["CAPTURE_SIMPLECOV_AT_RUNTIME"]
   # We use a random component here so that when we call `rails g super_scaffold` multiple times
   # we merge that coverage info instead of clobbering it.
   SimpleCov.command_name "runtime-simplecov-#{SecureRandom.hex}"
+  SimpleCov.external_at_exit = true
   SimpleCov.start "rails"
+  Rails.application.eager_load!
 end
 
 # Load the Rails application.
