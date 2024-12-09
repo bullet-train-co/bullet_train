ef75c378062900d027a873d76dfad6ae5dd3c86c
Jeremy Green <jeremy@octolabs.com>
2024-12-09 10:09:03 -0600

    can we get some coverage data about super scaffolding

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,9 @@
+if ENV["CAPTURE_SIMPLECOV_AT_RUNTIME"]
+  require "simplecov"
+  SimpleCov.command_name ENV["SIMPLECOV_RUNTIME_COMMAND_NAME"] || "fake-command-for-simplecov"
+  SimpleCov.start "rails"
+end
+
 # Load the Rails application.
 require_relative "application"
 
