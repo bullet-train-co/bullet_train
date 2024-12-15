2fd774ae0e44a1fa4120b0b6a91ddc766f1d17f0
Rich Steinmetz <hey@richsteinmetz.com>
2024-12-15 22:56:58 +0100

    Make turned off redis/pg non-breaking during setup
    It's possible that a dev installs Postgres and Redis as prompted which will pass the installed
    check. However, if the dev has not turned them on, they will fail later during the install, which
    can lead to confusion, frustration and less activations on BT.

--- bin/setup-scripts/check_redis.rb
+++ bin/setup-scripts/check_redis.rb
@@ -14,6 +14,20 @@ puts ""
 if /redis/.match?(redis_version_info.downcase)
   redis_version = redis_version_info.split("\s")[1]
   puts "You have redis #{redis_version} installed.".green
+
+  redis_running = system("redis-cli ping > /dev/null 2>&1")
+  if redis_running
+    puts "Redis is running.".green
+  else
+    puts "Redis is installed but not running. You will need it when starting your development app.".red
+    stop_now = ask_boolean "Would you like to stop here and start Redis first?", "n"
+    if stop_now
+      puts "Redis is not running and you've chosen to start it first. Run bin/setup again after.".red
+      exit
+    else
+      puts "Continuing without Redis running. You will need it when starting your development app.".yellow
+    end
+  end
 else
   puts "You don't seem to have redis installed.".red
   continue_anyway = ask_boolean "Would you like to try to continue without redis?", "n"
