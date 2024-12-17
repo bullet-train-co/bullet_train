1c4777e1eb87fedca7f0c6a595cc7282280c7106
Jeremy Green <jeremy@octolabs.com>
2024-12-17 13:31:22 -0600

    Maybe we can just piggy-back off of the MembershipsController

--- app/controllers/account/invitations_controller.rb
+++ app/controllers/account/invitations_controller.rb
@@ -4,8 +4,9 @@ class Account::InvitationsController < Account::ApplicationController
   private
 
   def permitted_membership_fields
-    [
-      # 🚅 super scaffolding will insert new fields above this line.
-    ]
+    Account::MembershipsController.new.send(:permitted_fields)
+    #[
+      ## 🚅 super scaffolding will insert new fields above this line.
+    #]
   end
 end
