d4e3462b5088eed6c9dfe8fe2ba05a7113d270d3
Jeremy Green <jeremy@octolabs.com>
2024-12-17 14:13:20 -0600

    some clean up

--- app/controllers/account/invitations_controller.rb
+++ app/controllers/account/invitations_controller.rb
@@ -4,9 +4,11 @@ class Account::InvitationsController < Account::ApplicationController
   private
 
   def permitted_membership_fields
+    # We render app/views/account/memberships/_attributes.html.erb on the Invitation form
+    # so by default we permit all of the fields that the MembershipController permits. If
+    # you want more control you can remove the following line and then return an array of
+    # membership attributes that you want to allow at the time of iniviation. For example:
+    # [ :description ]
     Account::MembershipsController.new.send(:permitted_fields)
-    #[
-      ## 🚅 super scaffolding will insert new fields above this line.
-    #]
   end
 end
