58cc4fdb598b53ec2dbea6b800c7063efefdb7c9
Jeremy Green <jeremy@octolabs.com>
2024-12-19 10:31:47 -0600

    maybe we'll need to run migrations in the setup

--- test/system/super_scaffolding/test_site/setup.rb
+++ test/system/super_scaffolding/test_site/setup.rb
@@ -21,6 +21,9 @@ class Setup < SuperScaffoldingTestSetup
       else
         puts `sed -i "s/raise .*/team\.memberships/g" app/models/test_site.rb`
       end
+
+      # TODO: Do we want to include this here?
+      # puts `bundle exec rails db:schema:load db:migrate db:test:prepare`
     end
   end
 end
