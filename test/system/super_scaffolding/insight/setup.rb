a1e82f96fe77cb5a61d8863cd29b62b15f19f252
Jeremy Green <jeremy@octolabs.com>
2024-12-19 11:28:09 -0600

    moar

--- test/system/super_scaffolding/insight/setup.rb
+++ test/system/super_scaffolding/insight/setup.rb
@@ -6,8 +6,8 @@ class Setup < SuperScaffoldingTestSetup
   # This allows us to define helper methods that aren't attached to thor commands
   no_commands do
     def setup
-      puts `rails g super_scaffold Projects::Step Team name:text_field description:trix_editor --navbar="ti-world"`
-      puts `rails g super_scaffold Objective Projects::Step,Team name:text_field description:trix_editor`
+      puts `rails g super_scaffold Insight Team name:text_field description:trix_editor --navbar="ti-world"`
+      puts `rails g super_scaffold Personality::CharacterTrait Insight,Team name:text_field description:trix_editor`
 
       # TODO: Do we want to include this here?
       # puts `bundle exec rails db:schema:load db:migrate db:test:prepare`
