4aaae5545e1c58c9f9a6a0cf02f878139d594b54
Jeremy Green <jeremy@octolabs.com>
2024-12-19 12:01:43 -0600

    extract another

---
+++ test/system/super_scaffolding/webhook/setup.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require_relative "../super_scaffolding_test_setup"
+
+class Setup < SuperScaffoldingTestSetup
+  # This allows us to define helper methods that aren't attached to thor commands
+  no_commands do
+    def setup
+      puts `rails g super_scaffold:incoming_webhooks SomeProvider`
+    end
+  end
+end
+
+# We create our own args array so that we don't have to ask the user to include `scaffolding_setup` on the command line
+args = ["scaffolding_setup"] + ARGV
+
+Setup.start(args)
