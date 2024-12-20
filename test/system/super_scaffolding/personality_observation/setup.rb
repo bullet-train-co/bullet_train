3cb3cb9fe169f2a12ed9ee7204a89ef0e13f7174
Jeremy Green <jeremy@octolabs.com>
2024-12-20 12:08:56 -0600

    this doesn't seem to do anything useful

--- test/system/super_scaffolding/personality_observation/setup.rb
+++ test/system/super_scaffolding/personality_observation/setup.rb
@@ -9,12 +9,16 @@ class Setup < SuperScaffoldingTestSetup
       puts `rails g super_scaffold Personality::Observation Team name:text_field description:trix_editor --navbar="ti-world"`
       puts `rails g super_scaffold Personality::Reactions::Response Personality::Observation,Team name:text_field description:trix_editor`
 
+      # TODO: This was pulled in from the bash script that we used to use for test setup. It changes instances of @response to
+      # @response_object in one of the test files generated above. The tests still pass even without. Seems unneccessary.
+      # Why did we ever do thi?
+      #
       # This tweaks a test file for some reason?
-      if Gem::Platform.local.os == "darwin"
-        puts `sed -i "" "s/\@response/\@response_object/g" test/controllers/api/v1/personality/reactions/responses_controller_test.rb`
-      else
-        puts `sed -i "s/\@response/\@response_object/g" test/controllers/api/v1/personality/reactions/responses_controller_test.rb`
-      end
+      # if Gem::Platform.local.os == "darwin"
+      #   puts `sed -i "" "s/\@response/\@response_object/g" test/controllers/api/v1/personality/reactions/responses_controller_test.rb`
+      # else
+      #   puts `sed -i "s/\@response/\@response_object/g" test/controllers/api/v1/personality/reactions/responses_controller_test.rb`
+      # end
     end
   end
 end
