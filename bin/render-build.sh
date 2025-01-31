69b22ee76ebad1807dcaeda70d7d81f6d81a501d
Jeremy Green <jeremy@octolabs.com>
2025-01-31 11:22:17 -0600

    Run `corepack enable` on render
    Fixes https://github.com/bullet-train-co/bullet_train/issues/1638

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,11 +2,16 @@
 # exit on error
 set -o errexit
 
+corepack enable
+yarn
+
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 
-# These are not combined because we want to reload all models after the migrations take place.
-# TODO Any way to read these from the `Procfile` where we have the same thing defined for Heroku?
-bundle exec rails db:migrate 
-bundle exec rails db:seed
+if [[ -n "${RUN_MIGRATIONS+1}" ]]; then
+  # These are not combined because we want to reload all models after the migrations take place.
+  # TODO Any way to read these from the `Procfile` where we have the same thing defined for Heroku?
+  bundle exec rails db:migrate
+  bundle exec rails db:seed
+fi
