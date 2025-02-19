fe4f6ccb879c5cc1c69268da29f8bc4625d047cf
Jeremy Green <jeremy@octolabs.com>
2025-02-19 14:31:38 -0600

    Remove some whitespace
    We accidentally got some trailing whitespace introduced that the linter
    doesn't like. This removes it.

--- app/avo/resources/team.rb
+++ app/avo/resources/team.rb
@@ -16,7 +16,7 @@ class Avo::Resources::Team < Avo::BaseResource
     field :slug, as: :text
     field :being_destroyed, as: :boolean, hide_on: :forms
     field :time_zone, as: :select, options: -> {
-      ActiveSupport::TimeZone.all.map { |tz| ["(GMT#{tz.formatted_offset}) #{tz.name}", tz.name] }.to_h 
+      ActiveSupport::TimeZone.all.map { |tz| ["(GMT#{tz.formatted_offset}) #{tz.name}", tz.name] }.to_h
     }
     field :locale, as: :text
     field :users, as: :has_many, through: :memberships
