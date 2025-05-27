f938f6a18145ed49ffcc52bf3fe39eb6ca19e722
Sergey Andronov <sergey@andronov.me>
2025-05-27 14:20:01 +0200

    Add deactivated notification

--- test/mailers/previews/webhooks/outgoing/endpoint_mailer_preview.rb
+++ test/mailers/previews/webhooks/outgoing/endpoint_mailer_preview.rb
@@ -2,11 +2,22 @@ class Webhooks::Outgoing::EndpointMailerPreview < ActionMailer::Preview
   def deactivation_limit_reached
     endpoint = Webhooks::Outgoing::Endpoint.last ||
       Webhooks::Outgoing::Endpoint.new(
-        workspace: Workspace.first,
+        team: Team.first,
         name: "Test Endpoint",
         url: "https://example.com/webhook",
         event_type_ids: ["contact.updated"]
       )
     Webhooks::Outgoing::EndpointMailer.deactivation_limit_reached endpoint
+  end
+
+  def deactivated
+    endpoint = Webhooks::Outgoing::Endpoint.last ||
+      Webhooks::Outgoing::Endpoint.new(
+        team: Team.first,
+        name: "Test Endpoint",
+        url: "https://example.com/webhook",
+        event_type_ids: ["contact.updated"]
+      )
+    Webhooks::Outgoing::EndpointMailer.deactivated endpoint
   end
 end
