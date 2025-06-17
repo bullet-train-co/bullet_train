7538699965abdb36b140f956d0073d92d6ea5702
Sergey Andronov <sergey@andronov.me>
2025-06-17 17:51:48 +0200

    Remove one unused index for deliveries

--- db/migrate/20250528140801_add_index_for_webhooks_outgoing_deliveries.rb
+++ db/migrate/20250528140801_add_index_for_webhooks_outgoing_deliveries.rb
@@ -3,6 +3,5 @@ class AddIndexForWebhooksOutgoingDeliveries < ActiveRecord::Migration[8.0]
 
   def change
     add_index :webhooks_outgoing_deliveries, [:endpoint_id, :event_id], algorithm: :concurrently
-    add_index :webhooks_outgoing_deliveries, [:delivered_at, :created_at], algorithm: :concurrently
   end
 end
