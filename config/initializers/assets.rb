ab69dd3c88b7f9d16e6d9fe2447606be17cc4d9d
Jeremy Green <jeremy@octolabs.com>
2025-06-26 15:25:10 -0500

    clean up

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,51 +7,16 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # TODO: Is this the best place for this?
-if ENV['SPROCKETS_NO_EXPORT_CONCURRENT']
-  # Prevent timeouts
+if ENV["SPROCKETS_NO_EXPORT_CONCURRENT"]
+  # These things all help with multi-platform Docker builds in GitHub Actions.
+  #
+  # Regexp needs more time when compiling for a different platform
   Regexp.timeout = 60 * 5
 
-  puts "********************************************************************************"
-  puts "We are setting Sprockets.export_concurrent = false"
-  puts "Regexp.timeout = #{Regexp.timeout}"
-  puts "********************************************************************************"
   # This prevents asset precompilation from hanging indefinitely.
   # https://github.com/rails/sprockets/issues/640
   Sprockets.export_concurrent = false
 
   # https://github.com/capistrano/rails/issues/55
   Rails.application.config.assets.logger = Logger.new $stdout
-
-  # https://github.com/rails/sprockets/issues/640#issuecomment-1852558528
-  #class Sprockets::Manifest
-    ## Public: Find all assets matching pattern set in environment.
-    ##
-    ## Returns Enumerator of Assets.
-    #def find(*args)
-      #puts "==============================================================="
-      #puts "Using monkey patched find!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-      #puts "==============================================================="
-      #unless environment
-        #raise Error, "manifest requires environment for compilation"
-      #end
-
-      #return to_enum(__method__, *args) unless block_given?
-
-      #environment = self.environment.cached
-      #promises = args.flatten.map do |path|
-        #Concurrent::Promise.execute(executor: executor) do
-          #environment.find_all_linked_assets(path).uniq do |asset|
-            #yield asset
-          #end
-        #end
-      #end
-      #promises.each(&:wait!)
-
-      #nil
-    #end
-  #end
-else
-  puts "********************************************************************************"
-  puts "We are NOOOOOT setting Sprockets.export_concurrent = false"
-  puts "********************************************************************************"
 end
