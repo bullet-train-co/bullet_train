3827c3742f5162abcada865cbe0687a3ae563cef
Jeremy Green <jeremy@octolabs.com>
2025-07-11 14:49:56 -0500

    Revert "register the service worker"
    This reverts commit 78a6c360a9daf5b7e111672ae444f4253db282f3.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -24,24 +24,6 @@ require("@icon/themify-icons/themify-icons.css")
 import { trixEditor } from "@bullet-train/fields"
 trixEditor()
 
-// TODO: Where should this live?
-function registerServiceWorker() {
-  console.log('Registering service worker...');
-
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
-      console.log('Service worker registered:', registration);
-    }).catch((error) => {
-      console.error('Service worker registration failed:', error);
-    });
-  });
-}
-
-if ('serviceWorker' in navigator) {
-  registerServiceWorker();
-}
-
-
 // ✅ YOUR APPLICATION'S INCLUDES
 // If you need to customize your application's includes, this is the place to do it. This helps avoid merge
 // conflicts in the future when Rails or Bullet Train update their own default includes.
