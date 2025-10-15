0ab5d4150215fb7e1c711d8a044466cb4d413253
Pascal Laliberte <pascal@hey.com>
2025-10-15 11:16:58 -0400

    Theme config: add example property `force_color_scheme_to` (#2243)
    * theme.rb: add example for BulletTrain::Themes::Light.force_color_scheme_to
    
    * standard fix

--- config/initializers/theme.rb
+++ config/initializers/theme.rb
@@ -8,3 +8,7 @@ BulletTrain::Themes::Light.color = :blue
 #
 # The logo shown in the navbar. To tweak further, run `bin/resolve shared/menu/logo --eject`
 # BulletTrain::Themes::Light.show_logo_in_account = true
+
+# Force the color scheme to :light or :dark.
+# Defaults to nil, which offers users the ability to choose their preference in Account Details.
+# BulletTrain::Themes::Light.force_color_scheme_to = :light
