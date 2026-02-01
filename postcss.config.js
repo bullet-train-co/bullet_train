b3f36abdc1d48a9ea04ed3ceedb51dc6edd43a15
Andrew Culver <andrew.culver@gmail.com>
2026-01-31 20:44:36 -0800

    Upgrade to Tailwind CSS v4. (#2362)

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,7 @@
-const { execSync } = require("child_process");
-
-const postcssImportConfigFile = execSync(`bundle exec bin/theme postcss-import-config ${process.env.THEME}`).toString().trim()
-const postcssImportConfig = require(postcssImportConfigFile)
-
 module.exports = {
   plugins: [
-    require('postcss-import')(postcssImportConfig),
-    require('tailwindcss/nesting'),
-    require('tailwindcss'),
-    require('autoprefixer'),
+    require('@tailwindcss/postcss')({
+      base: __dirname,
+    }),
   ]
 }
