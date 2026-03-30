ad332c1cb66fe060e50c2d889aa38f4f77b51443
Jeremy Green <jeremy@octolabs.com>
2026-03-30 12:48:30 -0500

    fix expected wording in tests

--- test/system/authentication_test.rb
+++ test/system/authentication_test.rb
@@ -74,7 +74,7 @@ class AuthenticationSystemTest < ApplicationSystemTestCase
     # TODO: At some point when devise is updated their translations should capitalize password.
     # That will make this next line fail. At that point we should capitalize 'password' below and remove these comments.
     # See: https://github.com/heartcombo/devise/pull/5454
-    assert_text("Invalid Email Address or password.")
+    assert_text("Invalid email or password.")
 
     # try signing in with the valid credentials.
     fill_in "Your Email Address", with: "andrew.culver@gmail.com"
